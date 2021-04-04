ece32f308e27b89568ba1300b152ed6c9d1b875a
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2021-04-04 17:06:40 +0100

    added new font and ui elements

---
+++ Pet-the-damn-dog/Assets/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+
+    // Singleton.
+    static GameMaster instance;
+    static public GameMaster getInstance()
+    {
+        if (instance == null)
+            instance = new GameMaster();
+
+        return instance;
+    }
+
+    float clickPower;
+
+    public void addClickPower(float value) { clickPower += value; }
+    public float getClickPower() { return clickPower; }
+
+}
---
+++ Pet-the-damn-dog/Assets/PointsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PointsController : MonoBehaviour
+{
+    TextMeshProUGUI TotalLovePointsText;
+    TextMeshProUGUI PPSText;
+
+    float totalPoints;
+    float pps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TotalLovePointsText = GameObject.FindGameObjectWithTag("[UI]TotalLovePoints").GetComponent<TextMeshProUGUI>();
+        PPSText = GameObject.FindGameObjectWithTag("[UI]PointsPerSecond").GetComponent<TextMeshProUGUI>();
+        StartCoroutine(CalculatePointsPerSecond());
+    }
+
+    private void Update()
+    {
+        // UI Update.
+        TotalLovePointsText.text = totalPoints.ToString() + " Love Points";
+        PPSText.text = pps.ToString() + " PP/S (Pets per second)";
+    }
+
+    IEnumerator CalculatePointsPerSecond()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1.0f);   // Allow us to wait for seconds and also not use new alot inside a loop.
+        while (true)
+        {
+            addPointsToTotal(pps);                                  // Add ours points per second to the total points every second.
+            // Place your method calls
+            yield return waitForSeconds;                            // Wait the 1 second.
+        }
+    }
+
+    public void addPointsToTotal(float value) { totalPoints += value; }
+    public float getTotalPoints() { return totalPoints; }
+
+    public void addPointsPerSecond(float value) { pps += value; }
+    public float getPointsPerSecond() { return pps; }
+}
