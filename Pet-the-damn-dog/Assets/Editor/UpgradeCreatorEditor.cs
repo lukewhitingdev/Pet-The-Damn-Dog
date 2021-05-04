f4d62ab9346ed5629bcb42abca3ae9ac8415872c
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2021-05-04 17:40:53 +0100

    started making a better way to create upgrades

---
+++ Pet-the-damn-dog/Assets/Editor/UpgradeCreatorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(UpgradeCreator))]
+public class UpgradeCreatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //UpgradeCreator myTarget = (UpgradeCreator)target;
+        EditorGUILayout.HelpBox("This creates a prefab in the resources folder.", MessageType.Info);
+
+        if (GUILayout.Button("Create"))
+        {
+            Debug.Log("Create upgrade");
+        }
+    }
+}
