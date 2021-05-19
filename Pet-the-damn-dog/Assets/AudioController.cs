a0912f42234c72d741b4731e77f5ef852aaca7e2
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2021-05-19 21:37:27 +0100

    Started developing audio system

---
+++ Pet-the-damn-dog/Assets/AudioController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    /*
+     
+        Problems:
+        Need a way to save audio that we are using to disk. so we can edit them later.
+        Need a way to easily add audio clips probs similar to the UpgradeCreator.cs
+        Need a way to reference and activate specific audio clips that are pre-loaded on scene load.
+     
+
+        Ideal Solution:
+        Loads all sounds from file -> When we want them we call the AudioController.search("[AudioID]").play to play.
+
+
+     */
+}
